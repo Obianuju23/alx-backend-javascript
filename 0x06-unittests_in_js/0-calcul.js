9bc1de2b828dc5b70d3a9031f23c0998b24918ec
Obianuju23 <obianujunmoh@gmail.com>
2023-12-25 05:44:44 +0100

    Created a function named calculateNumber

---
+++ 0x06-unittests_in_js/0-calcul.js
@@ -0,0 +1,15 @@
+function calculateNumber(a, b) {
+  const num1 = Math.round(a);
+  const num2 = Math.round(b);
+  const sum = num1 + num2;
+  return sum;
+}
+
+module.exports = calculateNumber;function calculateNumber(a, b) {
+  const num1 = Math.round(a);
+  const num2 = Math.round(b);
+  const sum = num1 + num2;
+  return sum;
+}
+
+module.exports = calculateNumber;
