9a45b34c426ff4e479d908bcff969279c4c339f3
Obianuju23 <obianujunmoh@gmail.com>
2023-12-30 17:09:34 +0100

    added util file and created property named calculateNumber

---
+++ 0x06-unittests_in_js/utils.js
@@ -0,0 +1,39 @@
+const Utils = {
+  // Define the calculateNumber property
+  calculateNumber: (type, a, b) => {
+    if (type === 'SUM') {
+      return Math.round(a) + Math.round(b);
+    }
+    if (type === 'SUBTRACT') {
+      return Math.round(a) - Math.round(b);
+    }
+    if (type === 'DIVIDE') {
+      if (Math.round(b) === 0) {
+        return 'Error';
+      }
+      return Math.round(a) / Math.round(b);
+    }
+    return null;
+  },
+};
+// Export the Utils module
+module.exports = Utils;const Utils = {
+  // Define the calculateNumber property
+  calculateNumber: (type, a, b) => {
+    if (type === 'SUM') {
+      return Math.round(a) + Math.round(b);
+    }
+    if (type === 'SUBTRACT') {
+      return Math.round(a) - Math.round(b);
+    }
+    if (type === 'DIVIDE') {
+      if (Math.round(b) === 0) {
+        return 'Error';
+      }
+      return Math.round(a) / Math.round(b);
+    }
+    return null;
+  },
+};
+// Export the Utils module
+module.exports = Utils;
